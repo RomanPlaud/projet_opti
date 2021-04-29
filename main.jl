ad4b44e6ad8551b87670a38ac5efb6039134596c
clemgris <clemence.grislain@eleves.enpc.fr>
2021-04-29 18:12:34 +0200

    test

--- main.jl
+++ main.jl
@@ -9,11 +9,15 @@ main:
 
 include("Projet.jl")
 
-function question2_a()
+
+
+function questions()
+
     G_max = 200
     T = 10
     K = 0
     p = 0.9
+
     #V=[[0 for i in 0:T+1] for j in 0:G_max+1]
     V = zeros(Float64,(T+1,G_max+1)::Tuple)
     V[end,:]=K*ones(Int,G_max+1)
@@ -23,7 +27,7 @@ function question2_a()
     for t in  T:-1:1
         for g in 1:G_max+1
             V[t,g]=10^(5)
-            for a in 1:(G_max-g)
+            for a in 1:(G_max-g+1)
                 Q[g,a]=1+(p^a)*V[t+1,g+a]+(1-p^a)*V[t+1,g]
                 if Q[g,a]<V[t,g]
                     V[t,g]= Q[g,a]
@@ -33,14 +37,17 @@ function question2_a()
         end
     end
 
+    print("politique optimale pour atteindre ")
+    print(G_max)
+    print("en maximum ")
+    print(T)
+    print(" tours : ")
     println(pi)
 
+    println(simul(pi,p,G_max))
 end
 
-question2_a()
-
-
-
+questions()
 
 function test()
     L= [i for i in 1:10]
